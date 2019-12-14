b16b47db22d341484958349117fb72acfa1aacd4
Mat Sz <contact@matsz.dev>
2019-12-14 18:21:03 +0100

    added an option to edit todo items

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -6,10 +6,12 @@ import { TodoItemEntity } from '../../types/Entities';
 import { crudUpdate, crudDelete } from '../../API';
 import ButtonMenu from '../ButtonMenu';
 import Menu from '../Menu';
+import InlineForm from '../InlineForm';
 
 function TodoItem({ item, onUpdate } : { item: TodoItemEntity, onUpdate?: () => void }) {
     const [ checked, setChecked ] = useState(item.done);
     const [ menuHidden, setMenuHidden ] = useState(true);
+    const [ isBeingRenamed, setIsBeingRenamed ] = useState(false);
     
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setChecked(event.target.checked);
@@ -24,8 +26,28 @@ function TodoItem({ item, onUpdate } : { item: TodoItemEntity, onUpdate?: () => 
         setMenuHidden(hidden => !hidden);
     };
 
+    const startRenaming = () => {
+        setMenuHidden(true);
+        setIsBeingRenamed(true);
+    };
+
+    const stopRenaming = () => setIsBeingRenamed(false);
+
+    const updateItem = async (name: string) => {
+        stopRenaming();
+
+        await crudUpdate('todoitems', {
+            ...item,
+            name: name,
+        });
+
+        if (onUpdate)
+            onUpdate();
+    };
+
     const deleteItem = async () => {
         await crudDelete('todoitems', item);
+        
         if (onUpdate)
             onUpdate();
     };
@@ -40,9 +62,23 @@ function TodoItem({ item, onUpdate } : { item: TodoItemEntity, onUpdate?: () => 
                 checked={checked}
                 onChange={onChange}
             />
-            <span>{ item.name }</span>
+            { isBeingRenamed ?
+                <InlineForm
+                    defaultValue={item.name}
+                    onSave={updateItem}
+                    onCancel={stopRenaming}
+                />
+            :
+                <span onDoubleClick={startRenaming}>
+                    { item.name }
+                </span>
+            }
             <ButtonMenu onClick={openMenu} />
             <Menu hidden={menuHidden} actions={[
+                {
+                    title: "Rename",
+                    onClick: startRenaming,
+                },
                 {
                     title: "Delete",
                     onClick: deleteItem,
