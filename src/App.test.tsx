e0c970d137d151d2b644d6f6c0c57fed5d82c4cf
Mat Sz <contact@matsz.dev>
2019-12-20 22:38:49 +0100

    fixed the rendering test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import reducers from './reducers';
+import { setStore } from './API';
+
+const store = createStore(reducers);
+setStore(store);
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>, div);
+    ReactDOM.unmountComponentAtNode(div);
 });
